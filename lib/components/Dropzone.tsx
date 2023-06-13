288e18a95fa24c2c98ac1060bd4ab7e34b94476d
Janosch Herrmann <janosch@janoschsworkspace.com>
2023-06-13 12:05:53 +0200

    feat: setup dropzone and videoplayer components (#3)
    * chore: remove unnecessary assets
    
    * chore: install dependencies
    
    * feat: adjust tailwind config
    
    * feat: add dropzone and videoplayer components
    
    * fix: lint issues
    
    ---------
    
    Co-authored-by: Janosch Herrmann <janosch.herrmann@finn.auto>

---
+++ lib/components/Dropzone.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { useCallback } from 'react'
+import { FileRejection, useDropzone } from 'react-dropzone'
+import clsx from 'clsx'
+import { VideoIcon } from '@radix-ui/react-icons'
+
+const Dropzone = ({ handleDropVideos }: { handleDropVideos: (videos: File[]) => void }) => {
+  const MAX_FILES = 2
+  const MAX_FILE_SIZE = 50000000 // 50MB
+
+  const onDrop = useCallback((acceptedFiles: File[], fileRejections: FileRejection[]) => {
+    // Handle rejected files, by showing an error toast or something
+    console.log(fileRejections)
+
+    // Do something with the files
+
+    console.log(acceptedFiles)
+    handleDropVideos(acceptedFiles)
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    maxFiles: MAX_FILES,
+    maxSize: MAX_FILE_SIZE,
+    accept: { 'video/*': ['.mp4', '.mov'] }
+  })
+
+  return (
+    <div
+      {...getRootProps({
+        className: clsx(
+          'border border-dotted rounded-md p-4 text-center bg-zinc-50 flex flex-col',
+          isDragActive ? 'border-zinc-600' : 'border-zinc-200'
+        )
+      })}>
+      <input {...getInputProps()} />
+      <VideoIcon className='mx-auto h-6 w-6' />
+      {isDragActive ? (
+        <p>Drop the videos here ...</p>
+      ) : (
+        <p>Drag &#39;n&#39; drop your videos here, or click to select videos</p>
+      )}
+    </div>
+  )
+}
+
+export default Dropzone
