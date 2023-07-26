abf46445e82f2fb7b72efba441a3d47185787c0e
Janosch Herrmann <janosch.herrmann@finn.auto>
2023-07-26 21:16:51 +0200

    feat: add color invert filter

--- lib/components/VideoOptions.tsx
+++ lib/components/VideoOptions.tsx
@@ -3,7 +3,7 @@ import { useMixcutContext } from '../contexts/MixcutContext'
 import { Separator } from '@radix-ui/react-separator'
 import { Source } from '../types'
 import { Button } from './Button'
-import { filterBlackAndWhite, filterSepia, speedUp } from '../utils/ffmpeg'
+import { filterBlackAndWhite, filterSepia, speedUp, invertColors } from '../utils/ffmpeg'
 import { Toast } from './Toast'
 
 export const VideoOptions = ({ videoIndex }: { videoIndex: Source }) => {
@@ -24,8 +24,8 @@ export const VideoOptions = ({ videoIndex }: { videoIndex: Source }) => {
 
       mixcutContext.addToQueue(async () =>
         filterBlackAndWhite(mixcutContext.ffmpeg!, mixcutContext[videoIndex].file!)
-          .then((bwVideoFile) => {
-            mixcutContext.setVideoSource(videoIndex, bwVideoFile)
+          .then((processedFile) => {
+            mixcutContext.setVideoSource(videoIndex, processedFile)
           })
           .catch((error) => {
             alert(error)
@@ -43,8 +43,27 @@ export const VideoOptions = ({ videoIndex }: { videoIndex: Source }) => {
 
       mixcutContext.addToQueue(async () =>
         filterSepia(mixcutContext.ffmpeg!, mixcutContext[videoIndex].file!)
-          .then((SVideoFile) => {
-            mixcutContext.setVideoSource(videoIndex, SVideoFile)
+          .then((processedFile) => {
+            mixcutContext.setVideoSource(videoIndex, processedFile)
+          })
+          .catch((error) => {
+            alert(error)
+          })
+          .finally(() => {
+            // handle unsetting the processing state
+          })
+      )
+    }
+  }
+
+  const handleInvertColors = () => {
+    if (mixcutContext.ffmpeg) {
+      handleTransformation()
+
+      mixcutContext.addToQueue(async () =>
+        invertColors(mixcutContext.ffmpeg!, mixcutContext[videoIndex].file!)
+          .then((processedFile) => {
+            mixcutContext.setVideoSource(videoIndex, processedFile)
           })
           .catch((error) => {
             alert(error)
@@ -91,6 +110,10 @@ export const VideoOptions = ({ videoIndex }: { videoIndex: Source }) => {
 
           <Button className='w-full' onClick={handleSpeedUp}>
             Speed Up x2
+          </Button>
+
+          <Button className='w-full' onClick={handleInvertColors}>
+            Invert colors
           </Button>
         </div>
       </div>
